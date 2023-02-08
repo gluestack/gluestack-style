1daad3f6f41fd317a86bb737149c46a524d72e21
ankit-tailor <tailorankit3345@gmail.com>
2023-02-08 13:46:18 +0530

    feat: experiment story

---
+++ example/storybook/src/api/Experiment/Checkbox.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { View } from 'react-native';
+import { styled } from '@dank-style/react';
+import { Wrapper } from '../../components/Wrapper';
+const StyledPlatformProps = styled(
+  View,
+  {
+    h: '$40',
+    w: '$40',
+
+    _web: {
+      bg: '$red500',
+    },
+
+    _android: {
+      bg: '$green500',
+    },
+
+    _ios: {
+      bg: '$blue500',
+    },
+  },
+  {}
+);
+
+export function Checkbox({ ...args }) {
+  return (
+    <Wrapper>
+      <StyledPlatformProps {...args}></StyledPlatformProps>
+    </Wrapper>
+  );
+}
---
+++ example/storybook/src/api/Experiment/Experiment.stories.tsx
@@ -0,0 +1,10 @@
+import { Checkbox } from './Checkbox';
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Checkbox',
+  component: Checkbox,
+};
