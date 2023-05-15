ec1a06ce86391fcc8cd4d9718d022f85e71f3076
ankit-tailor <tailorankit3345@gmail.com>
2023-05-15 15:59:50 +0530

    feat: tailwind class resolver

--- packages/react/src/plugins/tailwind-compact/tailwind-compact.tsx
+++ packages/react/src/plugins/tailwind-compact/tailwind-compact.tsx
@@ -2,7 +2,58 @@ import React, { useMemo } from 'react';
 import type { IStyled, IStyledPlugin } from '../../createStyled';
 import { propertyTokenMap } from '../../propertyTokenMap';
 import { useStyled } from '../../StyledProvider';
-import { deepMerge } from '../../utils';
+import { deepMerge, setObjectKeyValue } from '../../utils';
+import { dankKeywords } from './utils';
+
+const resolvePsuedoSelectors = (className: string) => {
+  const classes = className.split(':');
+  const stlyedObjectKeys = [];
+
+  // const [prop, ...value] = classes;
+
+  for (const cls of classes) {
+    if (dankKeywords[cls]) {
+      stlyedObjectKeys.push(dankKeywords[cls]);
+    } else {
+      stlyedObjectKeys.push(cls);
+    }
+  }
+
+  return stlyedObjectKeys;
+};
+
+const resolveClassToStyle = (className: string) => {
+  const classname = resolvePsuedoSelectors(className);
+
+  const originalProperty = classname.pop();
+  const pseudoSelectors = classname;
+  const [property, ...value] = originalProperty.split('-');
+
+  // const classes = className.split('-');
+  // const stlyedObjectKeys = [];
+
+  // const [prop, ...value] = classes;
+
+  // for (const cls of classes) {
+  //   const isPsuedoSelector = cls.endsWith(':');
+
+  //   if (isPsuedoSelector) {
+  //     const psuedoSelector = cls.substring(0, cls.length - 1);
+
+  //     if (states[psuedoSelector]) {
+  //       stlyedObjectKeys.push(states[psuedoSelector]);
+  //     } else {
+  //       console.warn(className, 'is not a valid psuedo selector');
+  //     }
+  //   }
+  //   stlyedObjectKeys.push(cls);
+  // }
+
+  return {
+    styledObjectKeys: [...pseudoSelectors, property],
+    value: `$${value.join('')}`,
+  };
+};
 
 export class Tailwind implements IStyledPlugin {
   name: 'Tailwind';
@@ -30,17 +81,30 @@ export class Tailwind implements IStyledPlugin {
     this.name = 'Tailwind';
   }
 
-  inputMiddleWare(tailwindClasses: any) {
-    this.tailwindClassesToDankStyledObject(tailwindClasses);
+  inputMiddleWare(styledObject: any) {
+    const { className, ...rest } = styledObject;
+    const classNameResolvedStyleObject =
+      this.tailwindClassesToDankStyledObject(className);
+    return deepMerge(rest, classNameResolvedStyleObject);
   }
 
   tailwindClassesToDankStyledObject(tailwindClasses: string) {
-    // const classes = tailwindClasses.split(' ');
-    console.log(tailwindClasses, '_______');
+    const classes = tailwindClasses.split(' ');
+    const styledObject = {};
+
+    for (const cls of classes) {
+      const { styledObjectKeys, value } = resolveClassToStyle(cls);
+      setObjectKeyValue(styledObject, styledObjectKeys, value);
+    }
+    return styledObject;
   }
 
-  componentMiddleWare({ NewComp, extendedConfig }: any) {
+  componentMiddleWare({ NewComp }: any) {
     return React.forwardRef((props: any, ref: any) => {
+      const { className, ...restProps } = props;
+      const classNameResolvedStyleObject = this.inputMiddleWare({
+        className: className ?? '',
+      });
       // const styledContext = useStyled();
       // const CONFIG = useMemo(
       //   () => ({
@@ -54,7 +118,9 @@ export class Tailwind implements IStyledPlugin {
       //   componentExtendedConfig = deepMerge(CONFIG, extendedConfig);
       // }
 
-      return <NewComp ref={ref} {...props} />;
+      return (
+        <NewComp ref={ref} sx={classNameResolvedStyleObject} {...restProps} />
+      );
     });
   }
 }
---
+++ packages/react/src/plugins/tailwind-compact/utils.ts
@@ -0,0 +1,9 @@
+export const dankKeywords = {
+  active: ':active',
+  checked: ':checked',
+  disabled: ':disabled',
+  hover: ':hover',
+  focus: ':focus',
+  light: '_light',
+  dark: '_dark',
+} as any;
